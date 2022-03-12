2cfb7b9869608bd5bcfdd5d33c4d4e2b8b685266
Attila Starkenius <symphtom@hotmail.com>
2022-03-12 23:35:50 +0100

    Fixing CleaningModelRepository.cs 12.3.2022.

--- httpswwwhemfridse/Repositories/CleaningModelRepository.cs
+++ httpswwwhemfridse/Repositories/CleaningModelRepository.cs
@@ -9,7 +9,7 @@ namespace httpswwwhemfridse.Repositories
 {
     public class CleaningModelRepository
     {
-        List<CleaningModel> authors = new List<CleaningModel>()
+        List<CleaningModel> customers = new List<CleaningModel>()
         {
             /*11.3.2022. I commit and check out with message: "Creating CleaningModelRepository.cs 11.3.2022."*/
 
@@ -67,8 +67,50 @@ so I change it to existing file: */
     firstName = "Attila",
     zip = "16869",
     sqm = 44
+} };
+        /*11.3.2022. I commit and push with message: "Creating CleaningModel object 11.3.2022."*/
+        //new CleaningModel
+        //{
+        //    Id = 2,
+        //    FirstName = "Steve",
+        //    LastName = "Smith"
+        //}
+        //};
+        public CleaningModel GetCustomer(Image facebookProfilePicture)
+        {
+            return customers.FirstOrDefault(a => a.facebookProfilePicture == facebookProfilePicture);
+        }
+        public List<CleaningModel> GetCustomers(int pageNumber = 1)
+        {
+            int pageSize = 10;
+            int skip = pageSize * (pageNumber - 1);
+            if (customers.Count < pageSize)
+                pageSize = customers.Count;
+            return customers
+              .Skip(skip)
+              .Take(pageSize).ToList();
+        }
+        public bool Save(CleaningModel customer)
+        {
+            var result = customers.Where(a => a.facebookProfilePicture == customer.facebookProfilePicture);
+            if (result != null)
+            {
+                if (result.Count() == 0)
+                {
+                    customers.Add(customer);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+/*12.3.2022. I adjust code from: new CleaningModel
+{
+    Id = 1,
+    FirstName = "Joydip",
+    LastName = "Kanjilal"
 },
-/*11.3.2022. I commit and push with message: "Creating CleaningModel object 11.3.2022."*/
 new CleaningModel
 {
     Id = 2,
@@ -105,3 +147,55 @@ return false;
 }
 }
 }
+to: 
+new CleaningModel
+{
+    //facebookProfilePicture = https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpf1/v/t34.0-12/10555140_10201501435212873_1318258071_n.jpg?oh=97ebc03895b7acee9aebbde7d6b002bf&oe=53C9ABB0&__gda__=1405685729_110e04e71d9,
+/*   11.3.2022. Obviously     facebookProfilePicture = https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpf1/v/t34.0-12/10555140_10201501435212873_1318258071_n.jpg?oh=97ebc03895b7acee9aebbde7d6b002bf&oe=53C9ABB0&__gda__=1405685729_110e04e71d9,
+won't work becausefacebookProfilePicture is Image variable
+so I change it to existing file: 
+facebookProfilePicture = Image.FromFile("C:/Users/symph/source/repos/httpswwwpandycocampaignflyttform/httpswwwhemfridse/facebookProfilePicture.jpg"),
+    firstName = "Attila",
+    zip = "16869",
+    sqm = 44
+} };
+/*11.3.2022. I commit and push with message: "Creating CleaningModel object 11.3.2022."
+//new CleaningModel
+//{
+//    Id = 2,
+//    FirstName = "Steve",
+//    LastName = "Smith"
+//}
+//};
+public CleaningModel GetCustomer(Image facebookProfilePicture)
+{
+    return customers.FirstOrDefault(a => a.facebookProfilePicture == facebookProfilePicture);
+}
+public List<CleaningModel> GetCustomers(int pageNumber = 1)
+{
+    int pageSize = 10;
+    int skip = pageSize * (pageNumber - 1);
+    if (customers.Count < pageSize)
+        pageSize = customers.Count;
+    return customers
+      .Skip(skip)
+      .Take(pageSize).ToList();
+}
+public bool Save(CleaningModel customer)
+{
+    var result = customers.Where(a => a.facebookProfilePicture == customer.facebookProfilePicture);
+    if (result != null)
+    {
+        if (result.Count() == 0)
+        {
+            customers.Add(customer);
+            return true;
+        }
+    }
+    return false;
+}
+    }
+}
+
+I commit and push with message: "Fixing CleaningModelRepository.cs 12.3.2022."
+*/
