2090cf6782a770738933bacd4f7760223e445de6
Attila Starkenius <symphtom@hotmail.com>
2022-08-16 19:14:35 +0200

    16.8.2022.

---
+++ httpswwwhemfridse/Models/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace httpswwwhemfridse.Models
+{
+    public class SearchModel
+    {
+        ///<summary>
+        /// Gets or sets search word.
+        ///</summary>
+        public string searchWord { get; set; }
+    }
+}
