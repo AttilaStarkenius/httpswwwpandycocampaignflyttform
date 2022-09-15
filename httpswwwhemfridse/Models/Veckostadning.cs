11ea8a2483c26f8264d82f7279bb1e8641f1147b
Attila Starkenius <AttilaStarkenius@users.noreply.github.com>
2022-09-15 23:56:08 +0200

    15.9.2022. Commit all to Git     Changes from a new computer

---
+++ httpswwwhemfridse/Models/Veckostadning.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace httpswwwhemfridse.Models
+{
+    public class Veckostadning
+    {
+        public string zip { get; set; }
+        public string sqm { get; set; }
+    }
+}
